c10c132cfa23675384870bcaecfec4e8733f4010
MiguelGuevara25 <miguel_g1789@hotmail.com>
2023-06-21 18:13:05 -0500

    add label

--- trabajoFinal/Form1.cs
+++ trabajoFinal/Form1.cs
@@ -16,5 +16,10 @@ namespace trabajoFinal
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
